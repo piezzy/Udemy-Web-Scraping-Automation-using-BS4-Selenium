5890d3912b3c10d66a215a8ef3386d374f5e30f0
mise en scene <139216558+piezzy@users.noreply.github.com>
2025-03-08 21:27:44 +0700

    web bot

--- web_bot/trello_bot.py
+++ web_bot/trello_bot.py
@@ -2,13 +2,36 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 import time
 from datetime import date
 import os
 import json
 
+CHROME_DRIVER_PATH = os.path.join(os.getcwd(), "chromedriver.exe")
+OP = Options()
+# OP.add_argument("--headless")
+OP.add_argument("--start-maximized")
+service = Service(CHROME_DRIVER_PATH)
+DRIVER = webdriver.Chrome(service=service, options=OP)
+
+def login():
+    with open ("config.json", "r") as f:
+        credentials = json.load(f)
+        time.sleep(2)
+        DRIVER.find_element(By.XPATH, value="//a[contains(@href, 'id.atlassian.com/login')]").click()
+        print(credentials) # dbgging
+        
 def main():
-    print("Trello Bot")
-    
+    try:
+        DRIVER.get("https://trello.com/")
+        login()
+        input("bot operation completed, press any key to exit")
+        DRIVER.quit()
+        
+    except Exception as e:
+        print("Error: ", e)
+        
 if __name__ == "__main__":
     main()
