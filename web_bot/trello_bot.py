c99c26ccfcc7daa019b134adf06c75f8d8b396b7
mise en scene <139216558+piezzy@users.noreply.github.com>
2025-03-08 16:39:28 +0700

    setup web automation

---
+++ web_bot/trello_bot.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+from datetime import date
+import os
+import json
+
+def main():
+    print("Trello Bot")
+    
+if __name__ == "__main__":
+    main()
